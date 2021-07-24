90eb25dfb6783a83637546000bb29ebffd37ca7c
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-07-24 11:27:43 +0100

    revisited

--- src/shaders/star5/fragment.js
+++ src/shaders/star5/fragment.js
@@ -8,20 +8,25 @@ float lineSegment(vec2 p, vec2 a, vec2 b) {
     return step(0.05, length(pa - ba*h));
 }
 
+
 void main() 
 {
+
+    float side = 1.0;  // fragment has side length = 1.0
+    float r = side/2.0;
+    vec2 p = vec2(r);  // center of the fragment
     
-        float line_1 = 1.0 - lineSegment(gl_PointCoord, vec2(0.5, 0.5 + 0.658351875 * 0.5), vec2(0.5 + 0.618027443 * 0.5, 0.5 + 1.0 * 0.5));
-        float line_2 = 1.0 - lineSegment(gl_PointCoord, vec2(0.5 + 0.618027443 * 0.5, 0.5 + 1.0 * 0.5), vec2(0.5 + 0.5 * 0.5, 0.5 + 0.27637816 * 0.5));
-        float line_3 = 1.0 - lineSegment(gl_PointCoord, vec2(0.5 + 0.5 * 0.5, 0.5 + 0.27637816 * 0.5), vec2(0.5 + 1.0 * 0.5, 0.5 - 0.236080209 * 0.5));
-        float line_4 = 1.0 - lineSegment(gl_PointCoord, vec2(0.5 + 1.0 * 0.5, 0.5 - 0.236080209 * 0.5), vec2(0.5 + 0.309026865 * 0.5, 0.5 - 0.341634306 * 0.5));
-        float line_5 = 1.0 - lineSegment(gl_PointCoord, vec2(0.5 + 0.309026865 * 0.5, 0.5 - 0.341634306 * 0.5), vec2(0.5 + 0.0 * 0.5, 0.5 - 1.0 * 0.5));
-        float line_6 = 1.0 - lineSegment(gl_PointCoord, vec2(0.5 + 0.0 * 0.5, 0.5 - 1.0 * 0.5), vec2(0.5 - 0.309026865 * 0.5, 0.5 - 0.341634306 * 0.5));
-        float line_7 = 1.0 - lineSegment(gl_PointCoord, vec2(0.5 - 0.309026865 * 0.5, 0.5 - 0.341634306 * 0.5), vec2(0.5 - 1.0 * 0.5, 0.5 - 0.236080209 * 0.5));
-        float line_8 = 1.0 - lineSegment(gl_PointCoord, vec2(0.5 - 1.0 * 0.5, 0.5 - 0.236080209 * 0.5), vec2(0.5 - 0.5 * 0.5, 0.5 + 0.27637816 * 0.5));
-        float line_9 = 1.0 - lineSegment(gl_PointCoord, vec2(0.5 - 0.5 * 0.5, 0.5 + 0.27637816 * 0.5), vec2(0.5 - 0.618027443 * 0.5, 0.5 + 1.0 * 0.5));
-        float line_10 = 1.0 - lineSegment(gl_PointCoord, vec2(0.5 - 0.618027443 * 0.5, 0.5 + 1.0 * 0.5), vec2(0.5, 0.5 + 0.658351875 * 0.5));
-        float line_11 = 1.0 - lineSegment(gl_PointCoord, vec2(0.5, 0.5 + 0.658351875 * 0.5), vec2(0.5, 0.5 + 0.658351875 * 0.5));
+        float line_1  = 1.0 - lineSegment(gl_PointCoord, vec2(p.x, p.y + 0.658351875 * r), vec2(p.x + 0.618027443 * r, p.y + 1.0 * r));
+        float line_2  = 1.0 - lineSegment(gl_PointCoord, vec2(p.x + 0.618027443 * r, p.y + 1.0 * r), vec2(p.x + 0.5 * r, p.y + 0.27637816 * r));
+        float line_3  = 1.0 - lineSegment(gl_PointCoord, vec2(p.x + 0.5 * r, p.y + 0.27637816 * r), vec2(p.x + 1.0 * r, p.y - 0.236080209 * r));
+        float line_4  = 1.0 - lineSegment(gl_PointCoord, vec2(p.x + 1.0 * r, p.y - 0.236080209 * r),  vec2(p.x + 0.309026865 * r, p.y - 0.341634306 * r));
+        float line_5  = 1.0 - lineSegment(gl_PointCoord, vec2(p.x + 0.309026865 * r, p.y - 0.341634306 * r), vec2(p.x + 0.0 * r, p.y - 1.0 * r));
+        float line_6  = 1.0 - lineSegment(gl_PointCoord, vec2(p.x + 0.0 * r, p.y - 1.0 * r), vec2(p.x - 0.309026865 * r, p.y - 0.341634306 * r));
+        float line_7  = 1.0 - lineSegment(gl_PointCoord, vec2(p.x - 0.309026865 * r, p.y - 0.341634306 * r), vec2(p.x - 1.0 * r, p.y - 0.236080209 * r));
+        float line_8  = 1.0 - lineSegment(gl_PointCoord, vec2(p.x - 1.0 * r, p.y - 0.236080209 * r),  vec2(p.x - 0.5 * r, p.y + 0.27637816 * r));
+        float line_9  = 1.0 - lineSegment(gl_PointCoord, vec2(p.x - 0.5 * r, p.y + 0.27637816 * r), vec2(p.x - 0.618027443 * r, p.y + 1.0 * r));
+        float line_10 = 1.0 - lineSegment(gl_PointCoord, vec2(p.x - 0.618027443 * r, p.y + 1.0 * r),  vec2(p.x, p.y + 0.658351875 * r));
+        float line_11 = 1.0 - lineSegment(gl_PointCoord, vec2(p.x, p.y + 0.658351875 * r), vec2(p.x, p.y + 0.658351875 * r));
     
     float shaper = line_1 + line_2 + line_3 + line_4 + line_5 + line_6 + line_7 + line_8 + line_9 + line_10 + line_11;
 
@@ -62,3 +67,16 @@ void main()
         // float line_10 = 1.0 - lineSegment(vec2(0.5 - 0.618027443 * 0.5, 0.5 + 1.0 * 0.5), vec2(0.5, 0.5 + 0.658351875 * 0.5));
         // float line_11 = 1.0 - lineSegment(vec2(0.5, 0.5 + 0.658351875 * 0.5), vec2(0.5, 0.5 + 0.658351875 * 0.5));
         //
+
+
+        // float line_1  = 1.0 - lineSegment(vec2(p.x, p.y + 0.658351875 * r), vec2(p.x + 0.618027443 * r, p.y + 1.0 * r));
+        // float line_2  = 1.0 - lineSegment(vec2(p.x + 0.618027443 * r, p.y + 1.0 * r), vec2(p.x + 0.5 * r, p.y + 0.27637816 * r));
+        // float line_3  = 1.0 - lineSegment(vec2(p.x + 0.5 * r, p.y + 0.27637816 * r), vec2(p.x + 1.0 * r, p.y - 0.236080209 * r));
+        // float line_4  = 1.0 - lineSegment(vec2(p.x + 1.0 * r, p.y - 0.236080209 * r),  vec2(p.x + 0.309026865 * r, p.y - 0.341634306 * r));
+        // float line_5  = 1.0 - lineSegment(vec2(p.x + 0.309026865 * r, p.y - 0.341634306 * r), vec2(p.x + 0.0 * r, p.y - 1.0 * r));
+        // float line_6  = 1.0 - lineSegment(vec2(p.x + 0.0 * r, p.y - 1.0 * r), vec2(p.x - 0.309026865 * r, p.y - 0.341634306 * r));
+        // float line_7  = 1.0 - lineSegment(vec2(p.x - 0.309026865 * r, p.y - 0.341634306 * r), vec2(p.x - 1.0 * r, p.y - 0.236080209 * r));
+        // float line_8  = 1.0 - lineSegment(vec2(p.x - 1.0 * r, p.y - 0.236080209 * r),  vec2(p.x - 0.5 * r, p.y + 0.27637816 * r));
+        // float line_9  = 1.0 - lineSegment(vec2(p.x - 0.5 * r, p.y + 0.27637816 * r), vec2(p.x - 0.618027443 * r, p.y + 1.0 * r));
+        // float line_10 = 1.0 - lineSegment(vec2(p.x - 0.618027443 * r, p.y + 1.0 * r),  vec2(p.x, p.y + 0.658351875 * r));
+        // float line_11 = 1.0 - lineSegment(vec2(p.x, p.y + 0.658351875 * r), vec2(p.x, p.y + 0.658351875 * r));
